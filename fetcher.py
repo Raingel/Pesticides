78b08225a6bdd510d21c5f94bfc0fb92b3e54956
OU JIEHAO <allenstorm2005@gmail.com>
2022-01-08 21:14:15 +0800

    Initial commit

---
+++ fetcher.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import requests
+import re
+
+def drug_list (farm = 'A010101'):
+    URI = 'https://pesticide.baphiq.gov.tw/information/Query/BugUserange/?flag=&bug=&farm='+farm
+    try:
+        df = pd.read_html(URI)
+        return df[0]
+    except ValueError as e:
+        return pd.DataFrame()
+    
+HOME_URI = "https://pesticide.baphiq.gov.tw/information/Query/Bug"
+r = requests.get(HOME_URI)
+assert r.status_code == requests.codes.ok, "無法擷取農藥資訊網 " + HOME_URI
+
+#retrieve farm id list from homepage
+farm_list = re.findall(r'id="farmhidden_([\w]*)"', r.text)
+#loop over all farm id
+pesticide_df = pd.DataFrame()
+for farm in farm_list:
+    print ('Start downloading', farm)
+    df = drug_list(farm)
+    if (df.empty != True):
+        pesticide_df = pesticide_df.append(df)
+        print ('Success', farm)
+    else:
+        print ('Failed', farm)
+#remove duplicated records
+pesticide_df.drop_duplicates(inplace = True)
+pesticide_df.reset_index(inplace = True, drop = True)
+pesticide_df.to_csv('./data/pesticides.csv', encoding = 'utf-8-sig')
+
