3f51126b7e95576b1617844687fded16d6091b4c
OU JIEHAO <allenstorm2005@gmail.com>
2022-01-09 14:06:39 +0800

    Fetch label info

--- fetcher.py
+++ fetcher.py
@@ -18,9 +18,17 @@ assert r.status_code == requests.codes.ok, "無法擷取農藥資訊網 " + HOME
 farm_list = re.findall(r'id="farmhidden_([\w]*)"', r.text)
 #loop over all farm id
 pesticide_df = pd.DataFrame()
-for farm in farm_list:
+for farm in farm_list[0:10]:
     print ('Start downloading', farm)
-    df = drug_list(farm)
+    retry_counter = 0
+    while retry_counter<5:
+        try:
+            df = drug_list(farm)
+            break
+        except requests.HTTPError as e:
+            print (e, farm)
+            retry_counter += 1
+
     if (df.empty != True):
         pesticide_df = pesticide_df.append(df)
         print ('Success', farm)
@@ -29,5 +37,38 @@ for farm in farm_list:
 #remove duplicated records
 pesticide_df.drop_duplicates(inplace = True)
 pesticide_df.reset_index(inplace = True, drop = True)
+#overwrite the most recent file
 pesticide_df.to_csv('./data/pesticides.csv', encoding = 'utf-8-sig')
+#back up
+pesticide_df.to_csv('./data/pesticides_'+pd.to_datetime("today").strftime("%Y-%m-%d")+'.csv', encoding = 'utf-8-sig')
 
+
+#FETCHING products info
+def label_page_parse (match):
+    PAGE_PATH = match.group(1)
+    r = requests.get('https://pesticide.baphiq.gov.tw/'+PAGE_PATH)
+    IMAGE_LIST = re.findall(r'type=mark&url=([\w-]*.jpg)', r.text)
+    print ('Found images:', IMAGE_LIST)
+    for img_name in IMAGE_LIST:
+        try:
+            with open('./label_img/'+img_name, 'wb') as f:
+                img = requests.get('https://pesticide.baphiq.gov.tw/information/Query/ViewmarkDownload/?type=mark&url='+img_name)
+                f.write(img.content)
+            print (img_name, 'downloaded')
+        except Exception as e:
+            print (img_name, e)
+    return (','.join(IMAGE_LIST))
+
+REGISTERED_URI = 'https://pesticide.baphiq.gov.tw/information/Query/RegisterList/?regtid=&regtnostart=&regtnoend=&pestcd=&compna=&prodga=&psbkna=&psbkga=&pestna=&pestga=&cidecd=&pescnt=&type=1&pagesize=55660&newquery=true'
+r = requests.get(REGISTERED_URI)
+assert r.status_code == requests.codes.ok, "無法擷取標籤頁面 " + HOME_URI
+#parse label values and download label images
+parsed_page = re.sub(r'<a href="(/information/Query/RegisterViewMark/\?regtid=\d*&regtno=\d*)" class="btn-s" target="_blank">標示</a>',label_page_parse, r.text)
+df = pd.read_html(parsed_page)
+registered_df = df[1]
+registered_df.columns = df[0].columns
+registered_df.drop('使用範圍', axis=1, inplace=True)
+#overwrite the most recent file
+registered_df.to_csv('./data/registered.csv', encoding = 'utf-8-sig')
+#back up
+registered_df.to_csv('./data/registered_'+pd.to_datetime("today").strftime("%Y-%m-%d")+'.csv', encoding = 'utf-8-sig')
